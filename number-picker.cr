2bb8610f8adf3d8f182127ace1ab57213d9df159
artenlf <felipe.arten@gmail.com>
2023-07-24 18:19:18 -0300

    fix user_guess input type to int

--- number-picker.cr
+++ number-picker.cr
@@ -1,6 +1,5 @@
-cpu_number = Random.rand(1..10).to_s # this is temporally converted to a string will fix it later
-
-user_guess = nil
+cpu_number = Random.rand(1..10)
+user_guess = 0
 guess_count = 0
 guess_limit = 3
 out_of_guesses = false
@@ -8,7 +7,7 @@ out_of_guesses = false
 while user_guess != cpu_number && !out_of_guesses
   if guess_count < guess_limit
     puts "Can you guess what number from 1 to 10 I'm thinking about?"
-    user_guess = gets
+    user_guess = gets.not_nil!.to_i # To-Do: fix when user input is nil
     guess_count += 1
   else
     out_of_guesses = true
